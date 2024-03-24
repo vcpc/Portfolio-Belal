60fbc82e0760eaca72fd0761fbe1a2ef82c5914e
haji <muhamedhajajii@outlook.com>
2024-03-24 15:04:06 +0200

    ci - Add navbar ui functionality

--- src/app/core/components/header/navbar/navbar.component.ts
+++ src/app/core/components/header/navbar/navbar.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
+  navbarShowFlag: boolean = true;
+  closeNavBar(navbar: HTMLElement, navbarList: HTMLUListElement): void {
+    navbar.classList.remove('show');
+    navbarList.classList.add('d-none');
+    this.navbarShowFlag = true;
+  }
+  openNavBar(navbar: HTMLElement, navbarList: HTMLUListElement): void {
+    navbarList.classList.remove('d-none');
+    navbar.classList.add('show');
+    this.navbarShowFlag = false;
+  }
 }
