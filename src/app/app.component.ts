e137f2e5a39eb66d786f3efdc3065473e4ea64bd
marwanabubakr <dev@marobakr>
2024-03-28 12:28:14 +0200

    add home section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
+import { core } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CoreModule } from './core/core.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CoreModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Portfolio-Belal';
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, HomeComponent],
+  exports: [HomeComponent],
+})
+export class CoreModule {}
