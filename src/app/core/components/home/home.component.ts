4f09b991f792ffb7d48f399a4311df796381ff8b
marwanabubakr <dev@marobakr>
2024-03-30 12:34:14 +0200

    add qoute at home

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { QouteComponent } from '../qoute/qoute.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [QouteComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
+import { QouteComponent } from './components/qoute/qoute.component';
 
 @NgModule({
   declarations: [],
