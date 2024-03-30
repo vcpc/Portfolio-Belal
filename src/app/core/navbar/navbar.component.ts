7e2fd1cd4f1d9565e69fdd1edffad796cf20bc89
haji <muhamedhajajii@outlook.com>
2024-03-30 12:09:10 +0200

    feat(core/navbar): add navbar

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import {
   AfterViewInit,
   Component,
   ElementRef,
+  QueryList,
   Renderer2,
   ViewChild,
+  ViewChildren,
 } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-navbar',
@@ -15,12 +17,39 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent implements AfterViewInit {
+  navbarShowFlag: boolean = true;
+  siteLang: string = 'EN';
   constructor(
     private _BreakpointObserver: BreakpointObserver,
     private _Renderer2: Renderer2
   ) {}
+  @ViewChild('navbar') navbar!: ElementRef;
+  @ViewChild('navbarList') navbarList!: ElementRef;
+  @ViewChildren('navbar__link')
+  navbar__link!: QueryList<ElementRef>;
 
-  navbarShowFlag: boolean = true;
+  ngAfterViewInit(): void {
+    this.closeNavBarBox();
+  }
+
+  activeBtn(e: any): void {
+    this.navbar__link.toArray().forEach((el) => {
+      this._Renderer2.removeClass(el.nativeElement, 'active');
+    });
+    e.target.classList.add('active');
+  }
+
+  closeNavBarBox(): void {
+    this._BreakpointObserver
+      .observe(Breakpoints.HandsetPortrait)
+      .subscribe((result) => {
+        if (!result.matches) {
+          this.navbarShowFlag = true;
+          this._Renderer2.removeClass(this.navbar.nativeElement, 'show');
+          this._Renderer2.addClass(this.navbarList.nativeElement, 'd-none');
+        }
+      });
+  }
   closeNavBar(navbar: HTMLElement, navbarList: HTMLUListElement): void {
     navbar.classList.remove('show');
     navbarList.classList.add('d-none');
@@ -31,20 +60,16 @@ export class NavbarComponent implements AfterViewInit {
     navbar.classList.add('show');
     this.navbarShowFlag = false;
   }
-
-  @ViewChild('navbar') navbar!: ElementRef;
-  @ViewChild('navbarList') navbarList!: ElementRef;
-
-  ngAfterViewInit(): void {
-    this._BreakpointObserver
-      .observe(Breakpoints.HandsetPortrait)
-      .subscribe((result) => {
-        console.log(result);
-        if (!result.matches) {
-          this.navbarShowFlag = true;
-          this._Renderer2.removeClass(this.navbar.nativeElement, 'show');
-          this._Renderer2.addClass(this.navbarList.nativeElement, 'd-none');
-        }
-      });
+  openLangDropdown(el: HTMLDivElement): void {
+    el.classList.toggle('show');
+    el.classList.toggle('d-none');
+  }
+  closeLangDropdown(el: HTMLDivElement, e: any): void {
+    const currentLang = this.siteLang;
+    const changedLanguage = e.target.innerHTML;
+    e.target.innerHTML = currentLang;
+    this.siteLang = changedLanguage;
+    el.classList.remove('show');
+    el.classList.add('d-none');
   }
 }
