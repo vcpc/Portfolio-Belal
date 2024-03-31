fc23dad775f9811971efd747181a668049b2eae3
marwanabubakr <dev@marobakr>
2024-03-31 19:25:47 +0200

    home

---
+++ src/app/core/components/qoute/qoute.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qoute',
+  standalone: true,
+  imports: [],
+  templateUrl: './qoute.component.html',
+  styleUrl: './qoute.component.scss'
+})
+export class QouteComponent {
+
+}
