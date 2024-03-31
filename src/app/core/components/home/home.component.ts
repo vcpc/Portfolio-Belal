a6b48e5c54fde7aec576a1d42488e2e08e423997
marwanabubakr <dev@marobakr>
2024-03-31 15:39:44 +0200

    add quote

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { QouteComponent } from '../qoute/qoute.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [QouteComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
