0e287b5cd7085c74925863532f16616da466f916
marwanabubakr <dev@marobakr>
2024-03-31 14:39:07 +0200

    add home

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { QouteComponent } from '../qoute/qoute.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [QouteComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
