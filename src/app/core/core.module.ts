e0545a018adf03568482c359f3084ccde634fe09
marwanabubakr <dev@marobakr>
2024-04-01 16:03:21 +0200

    quot

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MedialineComponent } from './components/medialine/medialine.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { QuoteComponent } from './components/quote/quote.component';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MedialineComponent, NavbarComponent],
-  exports: [MedialineComponent , NavbarComponent],
+  imports: [CommonModule, MedialineComponent, NavbarComponent, QuoteComponent],
+  exports: [MedialineComponent, NavbarComponent, QuoteComponent],
 })
 export class CoreModule {}
