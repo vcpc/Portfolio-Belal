457e304846d6491b0940a508213ddce462b0e977
marwanabubakr <dev@marobakr>
2024-04-01 17:03:00 +0200

    home

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MedialineComponent } from './components/medialine/medialine.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MedialineComponent, NavbarComponent],
-  exports: [MedialineComponent , NavbarComponent],
+  imports: [CommonModule, MedialineComponent, NavbarComponent, HomeComponent],
+  exports: [MedialineComponent, NavbarComponent, HomeComponent],
 })
 export class CoreModule {}
