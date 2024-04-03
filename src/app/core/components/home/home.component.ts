c125f825007dde9957393bdf3a8ba56dbf602412
marwanabubakr <dev@marobakr>
2024-04-03 14:05:36 +0200

    refactor home

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,12 +1,24 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  smallMedia: boolean = false;
 
+  constructor(private responsive: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.responsive.observe(Breakpoints.HandsetPortrait).subscribe((result) => {
+      if (result.matches) {
+        this.smallMedia = true;
+      }
+    });
+  }
 }
