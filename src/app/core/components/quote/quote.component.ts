c068b7fe9308a804e8feb89ec192b86cbf87d240
marwanabubakr <dev@marobakr>
2024-04-04 23:58:10 +0200

    add quote

---
+++ src/app/core/components/quote/quote.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Breakpoints } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-quote',
+  standalone: true,
+  imports: [],
+  templateUrl: './quote.component.html',
+  styleUrl: './quote.component.scss',
+})
+export class QuoteComponent {}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,22 @@ import { CommonModule } from '@angular/common';
 import { MedialineComponent } from './components/medialine/medialine.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { QuoteComponent } from './components/quote/quote.component';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MedialineComponent, NavbarComponent, FooterComponent],
-  exports: [MedialineComponent, NavbarComponent, FooterComponent ],
+  imports: [
+    CommonModule,
+    MedialineComponent,
+    NavbarComponent,
+    FooterComponent,
+    QuoteComponent,
+  ],
+  exports: [
+    MedialineComponent,
+    NavbarComponent,
+    FooterComponent,
+    QuoteComponent,
+  ],
 })
 export class CoreModule {}
