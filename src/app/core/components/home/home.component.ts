470fb294825315ebcf77d5ada11489523ddd6a51
marwanabubakr <dev@marobakr>
2024-04-04 20:53:14 +0200

    add home

---
+++ src/app/core/components/home/home.component.ts
@@ -0,0 +1,32 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  mobileMedia: boolean = false;
+
+  constructor(private _breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.handleMobile();
+  }
+
+  handleMobile() {
+    this._breakpointObserver
+      .observe(Breakpoints.HandsetPortrait)
+      .subscribe((result) => {
+        if (result.matches) {
+          this.mobileMedia = true;
+        } else {
+          this.mobileMedia = false;
+        }
+      });
+  }
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,22 @@ import { CommonModule } from '@angular/common';
 import { MedialineComponent } from './components/medialine/medialine.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MedialineComponent, NavbarComponent, FooterComponent],
-  exports: [MedialineComponent, NavbarComponent, FooterComponent ],
+  imports: [
+    CommonModule,
+    MedialineComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+  ],
+  exports: [
+    MedialineComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+  ],
 })
 export class CoreModule {}
