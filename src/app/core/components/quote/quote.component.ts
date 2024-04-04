9ee4f62bfff56b146c70161f0a002f610dfec46e
marwanabubakr <dev@marobakr>
2024-04-05 01:04:56 +0200

    add quote

--- src/app/core/components/quote/quote.component.ts
+++ src/app/core/components/quote/quote.component.ts
@@ -1,28 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-quote',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './quote.component.html',
   styleUrl: './quote.component.scss',
 })
 export class QuoteComponent implements OnInit {
   constructor(private _breakpointObserver: BreakpointObserver) {}
   mobileMedia: boolean = false;
+  minSmallMedia: boolean = false;
   ngOnInit() {
     this.handleMobile();
   }
 
   handleMobile() {
     this._breakpointObserver
-      .observe(Breakpoints.HandsetPortrait)
+      .observe([Breakpoints.HandsetPortrait, Breakpoints.Small])
       .subscribe((result) => {
         if (result.matches) {
-          this.mobileMedia = true;
+          if (result.breakpoints[Breakpoints.HandsetPortrait]) {
+            this.mobileMedia = true;
+            this.minSmallMedia = false;
+          }
+          if (result.breakpoints[Breakpoints.Small]) {
+            this.minSmallMedia = true;
+            this.mobileMedia = false;
+          }
         } else {
           this.mobileMedia = false;
+          this.minSmallMedia = false;
         }
       });
   }
