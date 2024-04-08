81b3d0298bc8eaa11a551e50c78dcf8c71d37ec2
haji <muhamedhajajii@outlook.com>
2024-04-08 10:54:48 +0200

    feat(@core): add routing

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,33 +1,13 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { QuoteComponent } from '../quote/quote.component';
+import { HeroComponent } from '../../../hero/hero.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, QuoteComponent],
+  imports: [CommonModule, HeroComponent, QuoteComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-  handsetPortrait: boolean = false;
-
-  constructor(private _breakpointObserver: BreakpointObserver) {}
-
-  ngOnInit() {
-    this.handleMobile();
-  }
-
-  handleMobile() {
-    this._breakpointObserver
-      .observe(Breakpoints.HandsetPortrait)
-      .subscribe((result) => {
-        if (result.matches) {
-          this.handsetPortrait = true;
-        } else {
-          this.handsetPortrait = false;
-        }
-      });
-  }
-}
+export class HomeComponent {}
---
+++ src/app/hero/hero.component.ts
@@ -0,0 +1,32 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.scss',
+})
+export class HeroComponent {
+  handsetPortrait: boolean = false;
+
+  constructor(private _breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.handleMobile();
+  }
+
+  handleMobile() {
+    this._breakpointObserver
+      .observe(Breakpoints.HandsetPortrait)
+      .subscribe((result) => {
+        if (result.matches) {
+          this.handsetPortrait = true;
+        } else {
+          this.handsetPortrait = false;
+        }
+      });
+  }
+}
