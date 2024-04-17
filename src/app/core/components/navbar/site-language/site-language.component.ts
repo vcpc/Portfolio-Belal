9577c3720677fd0ad1dd6ba4530406071f9436b0
haji <muhamedhajajii@outlook.com>
2024-04-17 17:46:42 +0200

    docs(@core/navbar/site-language): add documentation

--- src/app/core/components/navbar/site-language/site-language.component.ts
+++ src/app/core/components/navbar/site-language/site-language.component.ts
@@ -9,14 +9,46 @@ import { Component, Input } from '@angular/core';
   styleUrl: './site-language.component.scss',
 })
 export class SiteLanguageComponent {
+  /**
+   * @property {string} siteLang - the init language for the site
+   * @author Muhamed Hajajii
+   */
   siteLang: string = 'EN';
+
+  /**
+   * @property {Boolean} isLangDropDownOpen
+   * @see {@link animateLangChevron()}
+   * - Show and Hide the dropdown menu (Using SCSS Class 'show')
+   * - Play the animate for the chevron
+   * @author Muhamed Hajajii
+   */
   isLangDropDownOpen: Boolean = false;
+
+  /**
+   * @property {Boolean} isHandsetPortrait - handle padding in different screen width with the navbar li
+   * @author Muhamed Hajajii
+   */
   @Input() isHandsetPortrait: boolean = true;
 
+  /**
+   * Open language drop down menu
+   * @author Muhamed Hajajii
+   */
   onClickOpenLangDropdown(): void {
     this.animateLangChevron();
   }
-
+  /**
+   * Close language drop down menu
+   * @author Muhamed Hajajii
+   */
+  onClickCloseLangDropDown(): void {
+    this.animateLangChevron();
+  }
+  /**
+   * Change the current site language with the new language
+   * @param e - event to know which elemnt user selected
+   * @author Muhamed Hajajii
+   */
   onClickChangeSiteLang(e: any): void {
     const currentLang = this.siteLang;
     const changedLanguage = e.target.innerHTML;
@@ -24,16 +56,15 @@ export class SiteLanguageComponent {
     this.siteLang = changedLanguage;
     this.onClickCloseLangDropDown();
   }
-
+  /**
+   * animate the chevron svg while the dropdown menu appearing
+   * @author Muhamed Hajajii
+   */
   animateLangChevron(): void {
     if (this.isLangDropDownOpen == false) {
       this.isLangDropDownOpen = true;
     } else {
       this.isLangDropDownOpen = false;
     }
-  }
-
-  onClickCloseLangDropDown(): void {
-    this.animateLangChevron();
   }
 }
