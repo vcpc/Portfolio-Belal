caa9f51b956744ce5b14cef1581af4b7e447499a
marwanabubakr <dev@marobakr>
2024-04-29 10:38:05 +0300

    docs(@core/home/hero): add documentation

--- src/app/core/components/home/hero/hero.component.ts
+++ src/app/core/components/home/hero/hero.component.ts
@@ -2,23 +2,38 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+/**
+ * Hero component
+ * @author Marwan.A
+ */
 @Component({
   selector: 'app-hero',
   standalone: true,
   imports: [CommonModule, RouterLink],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss',
+  styleUrls: ['./hero.component.scss'],
 })
 export class HeroComponent {
+  /**
+   * Flag indicating whether the viewport is in handset portrait mode.
+   * @type {boolean}
+   */
   isHandsetPortrait: boolean = false;
 
   constructor(private _breakpointObserver: BreakpointObserver) {}
 
+  /**
+   * Initialize the component and start observing the handset portrait breakpoint.
+   * @returns {void}
+   */
   ngOnInit(): void {
     this.HandsetPortraitBreakpoint();
   }
 
+  /**
+   * Observe the handset portrait breakpoint and update the `isHandsetPortrait` flag accordingly when the user change display at screen.
+   * @returns {void}
+   */
   HandsetPortraitBreakpoint(): void {
     this._breakpointObserver
       .observe(Breakpoints.HandsetPortrait)
@@ -26,6 +41,12 @@ export class HeroComponent {
         this.updateHandsetPortrait(result.matches);
       });
   }
+
+  /**
+   * Update the `isHandsetPortrait` flag based on the given matches value.
+   * @param matches A boolean value indicating whether the viewport is in handset portrait mode.
+   * @returns {void}
+   */
   updateHandsetPortrait(matches: boolean): void {
     this.isHandsetPortrait = matches;
   }
