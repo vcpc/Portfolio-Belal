aeb0ab152e515115078cce6df5bdc0358465692b
haji <muhamedhajajii@outlook.com>
2024-05-13 10:31:42 +0300

    refactor(@core/navbar): fix code

--- src/app/core/components/navbar/navbar-anchors/dropdown-sitelanguages/dropdown-sitelanguages.component.ts
+++ src/app/core/components/navbar/navbar-anchors/dropdown-sitelanguages/dropdown-sitelanguages.component.ts
@@ -9,11 +9,27 @@ import { Component, Input } from '@angular/core';
   styleUrl: './dropdown-sitelanguages.component.scss',
 })
 export class DropdownSitelanguagesComponent {
+  /**
+   * @property {string} siteInitLang the defult sitelanguage
+   */
   siteInitLang: string = 'EN';
+  /**
+   * @property {string[]} restSiteLangs the rest site languages
+   */
   restSiteLangs: string[] = ['RU', 'UA'];
+  /**
+   * @property {boolean} isDropDownOpen use to control css classes to handle animation
+   */
   isDropDownOpen!: boolean;
+  /**
+   * @property {boolean} isNavbarOpen Input to handle navbar padding in small screen with the rest navbar anchors
+   */
   @Input() isNavbarOpen: Boolean = true;
-  openLangDropdown(e: any): void {
+  /**
+   *
+   * @param e passing the current site lang for {@link changeSiteLang} to handle current lang with the choosen lang
+   */
+  onDropDownClick(e: any): void {
     if (this.isDropDownOpen == true) {
       this.isDropDownOpen = false;
     } else {
@@ -21,7 +37,10 @@ export class DropdownSitelanguagesComponent {
     }
     this.changeSiteLang(e);
   }
-
+  /**
+   *
+   * @param e recieve the value for the current site lang and toggle it (just display)
+   */
   changeSiteLang(e: any): void {
     if (e.target.innerText != this.siteInitLang && e.target.innerText != '') {
       const currentLang = this.siteInitLang;
